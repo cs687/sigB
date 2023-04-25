6f0667d803962771fcc98e9c74dd3d427d5e6433
Christian Schwall <cs687@cam.ac.uk>
2023-04-25 17:46:36 +0100

    adding plotting files to repo

---
+++ plotting_sigB_data_2021.m
@@ -0,0 +1,31 @@
+function plotting_sigB_data_2021(varargin)
+
+date_in='2022-03-23';
+in_path_main='\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\SigB';
+D=dir([in_path_main,'\20*']);
+
+what_plot=nan;
+%checking input
+if ~isempty(varargin)
+    for i = 1:2:length(varargin)
+        theparam = lower(varargin{i});
+        switch(strtok(theparam))
+            case 'what'
+                what_plot=varargin{i+1};
+        end
+    end
+end
+if isnan(what_plot);
+    what_plot='MY';
+end
+
+
+for i=1:length(D);
+    in_path=[in_path_main,'\',D(i).name,'\subAuto\'];
+     p = initschnitz('Bacillus-01',date_in,'bacillus','rootDir',in_path,'imageDir',in_path);
+     p.dataDir=[p.rootDir,'Data\'];
+     if exist(p.dataDir)
+         plotting_MM_data_2021_11_01_v3(p,what_plot);
+         saveas(gcf,[D(i).name,'-',what_plot,'.png']);
+     end
+end
